cd6cdf70b72c0ef277ec508b6b092ca51ddc3644
abdulhaqani <ahaqa011@uottawa.ca>
2020-09-22 17:14:46 -0400

    users controller for signups

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  before_action :logged_redirect, only: %i[new create]
   def new; end
 
   def create
@@ -19,5 +20,14 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = 'You have successfully logged out'
     redirect_to login_path
+  end
+
+  private
+
+  def logged_redirect
+    if logged_in?
+      flash[:error] = 'You are already logged in'
+      redirect_to root_path
+    end
   end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:username, :password))
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = 'You have successfully signed up'
+      redirect_to root_path
+    else
+      render 'signup'
+    end
+  end
+
+  def destroy; end
+end
